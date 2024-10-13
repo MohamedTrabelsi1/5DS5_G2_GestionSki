f9c07e9d032f70521921f4bfc69793a090588e92
MohamedTrabelsi1 <trabelsi.mohamed@esprit.tn>
2024-10-13 16:35:44 +0100

    Third adjustment

--- src/test/java/tn/esprit/spring/test/PisteTest.java
+++ src/test/java/tn/esprit/spring/test/PisteTest.java
@@ -1,13 +1,9 @@
-package tn.esprit.spring;
+package tn.esprit.spring.test;
 
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-import io.swagger.v3.oas.annotations.Operation;
-import lombok.RequiredArgsConstructor;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
@@ -17,19 +13,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.ContextConfiguration;
+
 import org.springframework.test.web.servlet.MockMvc;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import tn.esprit.spring.GestionStationSkiApplication;
 import tn.esprit.spring.controllers.PisteRestController;
 import tn.esprit.spring.entities.Piste;
 import tn.esprit.spring.services.IPisteServices;
 
-import java.util.Arrays;
-import java.util.List;
-
-@SpringBootTest
+@SpringBootTest(classes = GestionStationSkiApplication.class)
 @AutoConfigureMockMvc
-@ContextConfiguration(classes = {GestionStationSkiApplication.class})
+
 public class PisteTest {
 
     @Autowired
