8b4c375aa9a4c52964e381e8122408272e0908b9
MohamedTrabelsi1 <trabelsi.mohamed@esprit.tn>
2024-10-14 21:22:21 +0100

    fix Crud v4.2

--- src/test/java/tn/esprit/spring/test/PisteTest.java
+++ src/test/java/tn/esprit/spring/test/PisteTest.java
@@ -101,6 +101,7 @@ public class PisteTest {
     void testDeleteById() throws Exception {
         Piste p ;
         List<Piste> pistes = pisteServices.retrieveAllPistes();
+        System.out.println(pistes);
         p=pistes.get(0);
 
         // Perform the DELETE request with path variable
